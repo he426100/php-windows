62af10c389ffb2b90fa1f97cf02147df865e9664
he426100 <{ID}+{username}@users.noreply.github.com>
2024-05-24 18:15:36 +0800

    恢复误删除的代码

--- libs/driver-win32/src/Lib/Kernel32.php
+++ libs/driver-win32/src/Lib/Kernel32.php
@@ -31,6 +31,47 @@ typedef const CHAR *LPCSTR;
 typedef const WCHAR *LPCWSTR;
 typedef void *HINSTANCE;
 typedef HINSTANCE HMODULE;
+typedef WCHAR *LPWSTR;
+typedef unsigned long DWORD;
+typedef unsigned long long  ULONG_PTR;
+typedef long  LONG;
+typedef void* PVOID;
+typedef PVOID HANDLE;
+typedef char TCHAR;
+typedef int BOOL;
+typedef const void *LPCVOID;
+typedef void* va_list;
+typedef HANDLE HLOCAL;
+
+typedef struct _PROCESSENTRY32 {
+    DWORD     dwSize;
+    DWORD     cntUsage;
+    DWORD     th32ProcessID;
+    ULONG_PTR th32DefaultHeapID;
+    DWORD     th32ModuleID;
+    DWORD     cntThreads;
+    DWORD     th32ParentProcessID;
+    LONG      pcPriClassBase;
+    DWORD     dwFlags;
+    TCHAR     szExeFile[260];
+} PROCESSENTRY32;
 
 HMODULE GetModuleHandleA(LPCSTR lpModuleName);
 HMODULE GetModuleHandleW(LPCWSTR lpModuleName);
+
+HANDLE CreateToolhelp32Snapshot(DWORD dwFlags, DWORD th32ProcessID);
+BOOL Process32First(HANDLE hSnapshot, PROCESSENTRY32 *lppe);
+BOOL Process32Next(HANDLE hSnapshot, PROCESSENTRY32 *lppe);
+BOOL CloseHandle(HANDLE hObject);
+DWORD GetLastError();
+
+DWORD FormatMessageW(
+  DWORD   dwFlags,
+  LPCVOID lpSource,
+  DWORD   dwMessageId,
+  DWORD   dwLanguageId,
+  LPWSTR  lpBuffer,
+  DWORD   nSize,
+  va_list *Arguments
+);
+void LocalFree(HLOCAL hMem);
